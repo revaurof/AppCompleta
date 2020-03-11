1883037d07fa0bfbe0364be3ee9a4b2c87cd4826
revaurof <revaurof@yahoo.com.br>
2020-03-11 09:20:14 -0300

    First commit

---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevIO.Business.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+       
+        public string Nome { get; set; }
+      
+        public string Descricao { get; set; }        
+ 
+        public string Imagem { get; set; }
+       
+        public decimal Valor { get; set; }      
+        
+        public DateTime DataCadastro { get; set; }
+        
+        public bool Ativo { get; set; }
+
+        /*EF Relation*/
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
